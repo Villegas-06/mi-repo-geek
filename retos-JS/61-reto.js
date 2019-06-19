be97c441ccb8029ed7146450ce1f462550cd83f9
villegas-06 <villegas4006@gmail.com>
2019-06-19 12:31:20 -0500

    retos-js(actualizado)

---
+++ retos-JS/61-reto.js
@@ -0,0 +1,13 @@
+// Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  // Change this value to test
+  testStrict(10);
+
+  /* Nota: el triple igual se usa para comparar entre números y strings 
+  para que la condición se cumpla correctamente */
